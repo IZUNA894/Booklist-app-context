70ebb70da29dfb0afee2a386c6b5bc84420df5e7
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-23 18:24:28 +0530

    Updates

---
+++ precache-manifest.79543ca9471c6e8c76f6ed7a2cd5f1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdf12c6f240c528974d58000a110c44",
+    "url": "/Booklist-app-context/index.html"
+  },
+  {
+    "revision": "7389f84bab30ae5f07d0",
+    "url": "/Booklist-app-context/static/css/main.06a2e2ad.chunk.css"
+  },
+  {
+    "revision": "fcb9f563cc066ce00991",
+    "url": "/Booklist-app-context/static/js/2.3b70eb2b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Booklist-app-context/static/js/2.3b70eb2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7389f84bab30ae5f07d0",
+    "url": "/Booklist-app-context/static/js/main.87dec965.chunk.js"
+  },
+  {
+    "revision": "8da794afda394a0bc3e3",
+    "url": "/Booklist-app-context/static/js/runtime-main.18bba954.js"
+  }
+]);
